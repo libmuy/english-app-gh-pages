9d91d97ae9c8b9135fcd07d50ea90d2542520b77
Libmuy <libmuy@gmail.com>
2024-10-30 19:55:21 +0900

    Publishing to gh-pages

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3862569030"
+    serviceWorkerVersion: "1131019419"
   }
 });
